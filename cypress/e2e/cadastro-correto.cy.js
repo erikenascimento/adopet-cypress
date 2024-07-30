98c0004f4fe4fb0ed2d7cdffb5fec920ddb55c1a
Erike Nascimento <nascimento.lf.erike@gmail.com>
2024-07-29 23:09:10 -0300

    adjusting the specs parameter to better reflect the good practices

---
+++ cypress/e2e/cadastro-correto.cy.js
@@ -0,0 +1,11 @@
+describe("Página de cadastro", () => {
+	it("Deve preencher os campos de formulário corretamente para cadastrar um novo usuário", () => {
+		cy.visit("https://adopet-frontend-cypress.vercel.app/");
+		cy.get('[data-test="register-button"]').click();
+		cy.get('[data-test="input-name"]').type("John Doe");
+		cy.get('[data-test="input-email"]').type("john@email.com");
+		cy.get('[data-test="input-password"]').type("Senha123");
+		cy.get('[data-test="input-confirm-password"]').type("Senha123");
+		cy.get('[data-test="submit-button"]').click();
+	});
+});
