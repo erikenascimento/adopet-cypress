524becce39d1f0e9f7c991e62e869233c1876d80
Erike Nascimento <101950160+erikenascimento@users.noreply.github.com>
2024-08-02 21:30:26 -0300

    Update exercise4.cy.js
    removing the bearer token because gitguardian cryied about it. But leaving the token public has no issues in this case since the website is just a test site for educational purposes.

--- cypress/e2e/exercise4.cy.js
+++ cypress/e2e/exercise4.cy.js
@@ -1,5 +1,5 @@
 describe("More exercises testing some API responses", () => {
-	const authorization = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMTY0M2NkNi03MTEyLTQxNWItOTVkMi0wNzkwNGIwZDFhMWMiLCJhZG9wdGVyTmFtZSI6IkFuYSBkZSBKZXN1cyIsImlhdCI6MTcyMjYzOTkzNSwiZXhwIjoxNzIyODk5MTM1fQ.dxuTOIHLpI3IZzo_hwhOMx9h5U_j8JAH6OjF11ThOq8`;
+	const authorization = `Bearer `;
 
 	it("Checks if the profile name on the API corresponds to: Ana de Jesus", () => {
 		cy.request({
